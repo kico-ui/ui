8e6aa175793c98abc37623d95c19da94593e3e5a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-21 14:22:36 +0530

    feat: add mode toggle component

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import * as React from "react"
+import { useTheme } from "next-themes"
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="h-9 w-8 px-0 focus-visible:ring-0 focus-visible:ring-transparent"
+        >
+          <SunIcon className="size-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute size-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
