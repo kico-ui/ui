3e01306aaae78c4173986784f53ec42f0f21ed4b
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-21 15:40:31 +0530

    chore: remove header links

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,18 +1,10 @@
 "use client"
 
-import React from "react"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-
 import MainNav from "@/components/main-nav"
 import MobileNav from "@/components/mobile-nav"
-import { Button } from "@/components/ui/button"
 import ModeToggle from "@/components/mode-toggle"
-import { cn } from "@/lib/utils"
 
 function SiteHeader() {
-  const pathname = usePathname()
-
   return (
     <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-between">
@@ -22,34 +14,6 @@ function SiteHeader() {
         </div>
         <div className="flex items-center space-x-2">
           <ModeToggle />
-          <Link href="/sign-up">
-            <Button
-              size="sm"
-              variant="outline"
-              className={cn(
-                "px-3 py-0 text-xs tracking-wider",
-                pathname === "/sign-up"
-                  ? "bg-accent text-foreground"
-                  : "text-foreground/60"
-              )}
-            >
-              Sign Up
-            </Button>
-          </Link>
-          <Link href="/login">
-            <Button
-              size="sm"
-              variant="outline"
-              className={cn(
-                "px-3 py-0 text-xs tracking-wider",
-                pathname === "/login"
-                  ? "bg-accent text-foreground"
-                  : "text-foreground/60"
-              )}
-            >
-              Login
-            </Button>
-          </Link>
         </div>
       </div>
     </header>
