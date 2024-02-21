2fc0139b49f336f01a058c0de35ef8fe414f8807
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-21 14:42:19 +0530

    feat: add home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,44 @@
+import Link from "next/link"
+
+import { Announcement } from "@/components/announcement"
+import { Icons } from "@/components/icons/icons"
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header"
+import { buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site-config"
+import { cn } from "@/lib/utils"
+
 export default function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <div className="container relative">
+      <PageHeader>
+        <Announcement />
+        <PageHeaderHeading>
+          Make <span className="text-active">development</span> easy
+        </PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps.Build complex components easy and responsive.
+        </PageHeaderDescription>
+        <PageActions>
+          <Link href="/components" className={cn(buttonVariants())}>
+            Get Started
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            <Icons.gitHub className="mr-2 size-4" />
+            GitHub
+          </Link>
+        </PageActions>
+      </PageHeader>
+    </div>
+  )
 }
