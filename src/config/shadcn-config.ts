57549ce249b59bec5b6af3a452e8203fb37a5aa7
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-22 11:51:47 +0530

    chore: change shadcn config

---
+++ src/config/shadcn-config.ts
@@ -0,0 +1,32 @@
+import { SidebarNavItem } from "@/types/nav"
+
+type ShadcnComponentConfigType = {
+  sidebarNav: SidebarNavItem[]
+}
+
+const shadcnComponentConfig: ShadcnComponentConfigType = {
+  sidebarNav: [
+    {
+      title: "Getting Started",
+      items: [{ title: "Introduction", href: "/components/shadcn", items: [] }],
+    },
+    {
+      title: "Shadcn Components",
+      items: [
+        {
+          title: "Sign up",
+          href: "/components/shadcn/sign-up",
+          items: [],
+        },
+
+        {
+          title: "Login",
+          href: "/components/shadcn/login",
+          items: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default shadcnComponentConfig
