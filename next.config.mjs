89567863c9dc5540bce8dc3d2edcd9d130061f19
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-22 18:53:53 +0530

    feat: add redirects links

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,35 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["shiki", "vscode-oniguruma"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/components/tailwindcss",
+        destination: "/components/tailwindcss/introduction",
+        permanent: true,
+      },
+      {
+        source: "/components/bootstrap",
+        destination: "/components/bootstrap/introduction",
+        permanent: true,
+      },
+      {
+        source: "/components/shadcn",
+        destination: "/components/shadcn/introduction",
+        permanent: true,
+      },
+      {
+        source: "/components/css",
+        destination: "/components/css/introduction",
+        permanent: true,
+      },
+      {
+        source: "/components/ant-design",
+        destination: "/components/ant-design/introduction",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withContentlayer(nextConfig)
