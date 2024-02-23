1521f5b546e1c6cf4a89e1d08e36ca103fffab3f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-23 21:48:24 +0530

    fix: blur animation

--- src/components/component-examples.tsx
+++ src/components/component-examples.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import BlurAnimation from "@/components/animations/blur-animation"
+// import BlurAnimation from "@/components/animations/blur-animation"
 import SiteFooterDemo from "@/registry/shadcn/footer-demo"
 import SignUpDemo from "@/registry/shadcn/sign-up-demo"
 
@@ -9,12 +9,12 @@ const ComponentExamples = () => {
     <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-8 mt-10">
       <div>
         <div className="max-w-[600px] rounded-md border">
-          <BlurAnimation className="bg-pink-800" />
+          {/* <BlurAnimation className="bg-pink-100 dark:bg-pink-800" /> */}
           <SiteFooterDemo />
         </div>
       </div>
       <div className="max-w-[600px] rounded-md border">
-        <BlurAnimation className="bg-blue-800 md:top-[50%]" />
+        {/* <BlurAnimation className="bg-blue-100 dark:bg-blue-800 md:top-[50%]" /> */}
         <SignUpDemo />
       </div>
     </div>
