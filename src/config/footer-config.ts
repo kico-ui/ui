39eef4625b3e7a1c8d65a15ed389d6dcea066a0c
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-23 17:34:52 +0530

    feat: add footer links config

---
+++ src/config/footer-config.ts
@@ -0,0 +1,57 @@
+import { siteConfig } from "@/config/site-config"
+
+export const resources = {
+  title: "resources",
+  items: [
+    {
+      title: "Components",
+      href: "/components",
+    },
+    {
+      title: "Docs",
+      href: "/docs",
+    },
+
+    {
+      title: "Examples",
+      href: "/examples",
+    },
+    {
+      title: "Learn",
+      href: "/learn",
+    },
+  ],
+}
+
+export type FooterLinksType = typeof resources
+
+export const more: FooterLinksType = {
+  title: "More",
+  items: [
+    { title: "Releases", href: "/releases" },
+    {
+      title: "GitHub",
+      href: siteConfig.links.github,
+    },
+  ],
+}
+
+export const aboutKico: FooterLinksType = {
+  title: "About Kico",
+  items: [
+    { title: "Open Source Software", href: siteConfig.links.github },
+    {
+      title: "GitHub",
+      href: siteConfig.links.github,
+    },
+    {
+      title: "X",
+      href: siteConfig.links.twitter,
+    },
+  ],
+}
+
+export const legal: FooterLinksType = {
+  title: "Legal",
+  items: [{ title: "Privacy Policy", href: "/privacy-policy" }],
+}
