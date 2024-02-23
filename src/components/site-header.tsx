ad715d9c5bb031f9e97a5bc8d8ba205f105a3d3a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-23 08:11:29 +0530

    feat: add github link

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,8 +1,13 @@
 "use client"
 
+import Link from "next/link"
 import MainNav from "@/components/main-nav"
 import MobileNav from "@/components/mobile-nav"
 import ModeToggle from "@/components/mode-toggle"
+import { siteConfig } from "@/config/site-config"
+import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons/icons"
+import { cn } from "@/lib/utils"
 
 function SiteHeader() {
   return (
@@ -12,7 +17,20 @@ function SiteHeader() {
           <MainNav />
           <MobileNav />
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center space-x-2">
+          <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+            <div
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                }),
+                "w-9 px-0"
+              )}
+            >
+              <Icons.gitHub className="size-4" />
+              <span className="sr-only">GitHub</span>
+            </div>
+          </Link>
           <ModeToggle />
         </div>
       </div>
