f64298fd2c1ee38e874927b4b51ac931c29b3811
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-23 10:42:17 +0530

    fix:  breadcrumb

--- src/app/components/shadcn/[slug]/page.tsx
+++ src/app/components/shadcn/[slug]/page.tsx
@@ -32,11 +32,13 @@ const ShadcnComponent = async ({ params }: ShadcnComponentParams) => {
     <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
       <div className="mx-auto w-full min-w-0">
         <div className="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
-          <div className="truncate">Components</div>
+          <div className="truncate">components</div>
           <ChevronRightIcon className="size-4" />
-          <div className="truncate">Shadcn</div>
+          <div className="truncate">shadcn</div>
           <ChevronRightIcon className="size-4" />
-          <div className="font-medium text-foreground">{component.title}</div>
+          <div className="font-medium text-foreground">
+            {component.breadcrumb}
+          </div>
         </div>
         <div className="space-y-2">
           <h1 className={cn("scroll-m-20 text-4xl font-bold tracking-tight")}>
