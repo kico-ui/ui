762dd756d9bdc794a2f9fae210c5877c8abe2f56
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-25 14:44:12 +0530

    feat: add effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,43 +17,46 @@ import BlurAnimation from "@/components/animations/blur-animation"
 
 export default function Home() {
   return (
-    <div className="container">
+    <section className="container">
       <div className="min-h-screen">
-        <PageHeader className="relative">
-          <BlurAnimation className="md:top-[30%] md:left-[30%] dark:bg-blue-600 bg-white" />
-          <Announcement />
-          <PageHeaderHeading>
-            Make <span className="text-active">development</span> easy
-          </PageHeaderHeading>
-          <PageHeaderDescription>
-            Beautifully designed components that you can copy and paste into
-            your apps. Build complex components easy and responsive.
-          </PageHeaderDescription>
-          <PageActions className="relative mt-4">
-            <Link href="/components" className={cn(buttonVariants())}>
-              Get Started
-            </Link>
-            <Link
-              target="_blank"
-              rel="noreferrer"
-              href={siteConfig.links.github}
-              className={cn(buttonVariants({ variant: "outline" }))}
-            >
-              <Icons.gitHub className="mr-2 size-4" />
-              GitHub
-            </Link>
-          </PageActions>
-        </PageHeader>
+        <div className="w-full relative">
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+          <PageHeader className="relative">
+            <BlurAnimation className="md:top-[30%] md:left-[30%] dark:bg-blue-600 bg-white" />
+            <Announcement />
+            <PageHeaderHeading>
+              Make <span className="text-active">development</span> easy
+            </PageHeaderHeading>
+            <PageHeaderDescription>
+              Beautifully designed components that you can copy and paste into
+              your apps. Build complex components easy and responsive.
+            </PageHeaderDescription>
+            <PageActions className="relative mt-4">
+              <Link href="/components" className={cn(buttonVariants())}>
+                Get Started
+              </Link>
+              <Link
+                target="_blank"
+                rel="noreferrer"
+                href={siteConfig.links.github}
+                className={cn(buttonVariants({ variant: "outline" }))}
+              >
+                <Icons.gitHub className="mr-2 size-4" />
+                GitHub
+              </Link>
+            </PageActions>
+          </PageHeader>
+        </div>
 
         <FeatureCards />
       </div>
 
-      <div className="min-h-screen relative pb-24">
+      <div className="container min-h-screen relative pb-24">
         <div className="flex items-center justify-center">
           <h2 className="text-2xl md:text-4xl">Kico ui component examples</h2>
         </div>
         <ComponentExamples />
       </div>
-    </div>
+    </section>
   )
 }
