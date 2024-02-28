09e56c775e95ed01e1ea67d314eb2c635b542f45
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-28 11:24:30 +0530

    refactor:  linked card component

--- src/components/ui/linked-card.tsx
+++ src/components/ui/linked-card.tsx
@@ -1,18 +1,36 @@
-import { cn } from "@/lib/utils"
-import Link from "next/link"
 import React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+
+type LinkedCardProps = {
+  disabled?: boolean
+} & React.ComponentProps<typeof Link>
 
 const LinkedCard = ({
   className,
+  disabled = false,
   ...props
-}: React.ComponentProps<typeof Link>) => (
-  <Link
-    className={cn(
-      "flex w-full flex-col items-center rounded-xl border bg-transparent p-6 text-card-foreground shadow transition-colors hover:bg-muted/50 sm:p-10",
-      className
+}: LinkedCardProps) => (
+  <Button
+    disabled={disabled}
+    className="size-full bg-transparent hover:bg-transparent relative"
+  >
+    {disabled && (
+      <Badge className="absolute top-6 left-8 bg-green-500/15 text-green-500">
+        Planned
+      </Badge>
     )}
-    {...props}
-  />
+    <Link
+      className={cn(
+        "flex w-full flex-col items-center rounded-xl border bg-transparent p-6 text-card-foreground shadow transition-colors hover:bg-muted/50 sm:p-10",
+        className
+      )}
+      {...props}
+    />
+  </Button>
 )
 LinkedCard.displayName = "LinkedCard"
 
