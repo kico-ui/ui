435aae967434efe7a7fe9163b027112838f2f356
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-02-28 11:04:08 +0530

    refactor: grid and blur effect

--- src/components/component-examples.tsx
+++ src/components/component-examples.tsx
@@ -3,27 +3,25 @@ import React from "react"
 import SiteFooterDemo from "@/registry/shadcn/footer-demo"
 import SignUpDemo from "@/registry/shadcn/sign-up-demo"
 import NavbarDemo from "@/registry/shadcn/navbar-demo"
+import { GridEffect } from "@/components/animations/grid-effect"
+import { DotEffect } from "@/components/animations/dot-effect"
 
 const ComponentExamples = () => {
   return (
-    <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-8 mt-10">
-      <div>
+    <div className="flex flex-col space-y-4 md:space-y-0 mt-10 md:grid md:grid-cols-2 gap-4">
+      <div className="space-y-4">
         <div className="rounded-md border relative">
-          <div className="absolute inset-0 z-10 size-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-            <div className="absolute inset-x-0 top-0 z-10 m-auto size-[310px] rounded-full bg-blue-600 opacity-20 blur-[100px]"></div>
-          </div>
+          <GridEffect />
           <SiteFooterDemo />
         </div>
 
-        <div className="rounded-md border mt-10">
+        <div className="rounded-md border">
           <NavbarDemo />
         </div>
       </div>
 
-      <div className="max-w-[600px] rounded-md border relative">
-        <div className="absolute inset-0 size-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#ffffff20_1px,transparent_1px)] [background-size:16px_16px]">
-          <div className="absolute inset-x-0 top-0 m-auto size-[310px] rounded-full opacity-20 blur-[100px]"></div>
-        </div>
+      <div className="rounded-md border relative">
+        <DotEffect />
         <SignUpDemo />
       </div>
     </div>
