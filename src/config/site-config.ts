4124192beda7050aa9d7d2729a4d9e9da344b627
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-08 09:55:21 +0530

    fix: site configuration

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,11 +1,9 @@
 export const siteConfig = {
   name: "kico/ui",
   url: "https://kico-ui.com",
-  ogImage: "https://ui.shadcn.com/og.jpg",
   description:
     "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
   links: {
-    twitter: "https://twitter.com/shadcn",
     github: "https://github.com/kico-ui/ui.git",
     releases: "https://github.com/kico-ui/ui/releases",
   },
