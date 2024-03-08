a9e2009526f66359f5f1a9bf515027184211897f
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2024-03-08 09:56:20 +0530

    feat: add kico fancy button demo

---
+++ src/registry/tailwindcss/kico-fancy-button-demo.tsx
@@ -0,0 +1,12 @@
+import { KicoFancyButton } from "@/components/kico/kico-fancy-button"
+
+const KicoFancyButtonDemo = () => {
+  return (
+    <div className="bg-black space-x-8">
+      <KicoFancyButton>Button</KicoFancyButton>
+      <KicoFancyButton white>Button</KicoFancyButton>
+    </div>
+  )
+}
+
+export default KicoFancyButtonDemo
